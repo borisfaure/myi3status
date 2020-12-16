483155211b02cf56a9fd821316b7b1f287f85d96
Julien Dessaux <julien.dessaux@adyxax.org>
2020-12-16 12:30:45 +0100

    go fmt

--- i3status.go
+++ i3status.go
@@ -1,84 +1,84 @@
 package main
+
 import (
-    "bufio"
-    "errors"
-    "flag"
-    "fmt"
-    "log"
-    "os/exec"
+	"bufio"
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"os/exec"
 )
 
 func main_loop(location *string, rain_color *string) error {
-    path, lookupErr := exec.LookPath("i3status")
-    if lookupErr != nil {
-        return lookupErr
-    }
-    cmd := exec.Command(path)
-    stdout, pipeErr := cmd.StdoutPipe()
-    if pipeErr != nil {
-        return pipeErr
-    }
-    if err := cmd.Start(); err != nil {
-        return err
-    }
-    scanner := bufio.NewScanner(stdout)
+	path, lookupErr := exec.LookPath("i3status")
+	if lookupErr != nil {
+		return lookupErr
+	}
+	cmd := exec.Command(path)
+	stdout, pipeErr := cmd.StdoutPipe()
+	if pipeErr != nil {
+		return pipeErr
+	}
+	if err := cmd.Start(); err != nil {
+		return err
+	}
+	scanner := bufio.NewScanner(stdout)
 
-    /* expect '{"version": 1}' */
-    scannerOk := scanner.Scan()
-    if !scannerOk {
-        return errors.New("scanner failed")
-    }
-    var t string
-    t = scanner.Text()
-    if t != "{\"version\":1}" {
-        return errors.New("invalid header '"+t+"'")
-    }
-    fmt.Println(t)
+	/* expect '{"version": 1}' */
+	scannerOk := scanner.Scan()
+	if !scannerOk {
+		return errors.New("scanner failed")
+	}
+	var t string
+	t = scanner.Text()
+	if t != "{\"version\":1}" {
+		return errors.New("invalid header '" + t + "'")
+	}
+	fmt.Println(t)
 
-    /* expect '[' */
-    scannerOk2 := scanner.Scan()
-    if !scannerOk2 {
-        return errors.New("scanner failed")
-    }
-    t = scanner.Text()
-    if t != "[" {
-        return errors.New("invalid 2nd line '"+t+"'")
-    }
-    fmt.Println(t)
+	/* expect '[' */
+	scannerOk2 := scanner.Scan()
+	if !scannerOk2 {
+		return errors.New("scanner failed")
+	}
+	t = scanner.Text()
+	if t != "[" {
+		return errors.New("invalid 2nd line '" + t + "'")
+	}
+	fmt.Println(t)
 
-    first := true
-    for scanner.Scan() {
-        text := scanner.Text()
-        if location != nil {
-            weather, errStatus := GetRainI3barFormat(location, rain_color)
-            if errStatus != nil {
-                fmt.Println(text)
-            } else if first {
-                fmt.Println("["+weather+","+text[1:])
-                first = false
-            } else {
-                fmt.Println(",["+weather+","+text[2:])
-            }
-        } else {
-            fmt.Println(text)
-        }
-    }
+	first := true
+	for scanner.Scan() {
+		text := scanner.Text()
+		if location != nil {
+			weather, errStatus := GetRainI3barFormat(location, rain_color)
+			if errStatus != nil {
+				fmt.Println(text)
+			} else if first {
+				fmt.Println("[" + weather + "," + text[1:])
+				first = false
+			} else {
+				fmt.Println(",[" + weather + "," + text[2:])
+			}
+		} else {
+			fmt.Println(text)
+		}
+	}
 
-    return nil
+	return nil
 }
 
 func main() {
-    location := flag.String("location", "",
-        "a location for the Pluie dans l'heure API, in the form 'lat=48.859333&lon=2.340591'")
-    rainColor := flag.String("rain_color", "#268bd2",
-        "Color to display text when it's raining")
-    flag.Parse()
-    if *location == "" {
-        location = nil
-    }
-    err := main_loop(location, rainColor)
-    if err != nil {
-        log.Fatal(err)
-    }
+	location := flag.String("location", "",
+		"a location for the Pluie dans l'heure API, in the form 'lat=48.859333&lon=2.340591'")
+	rainColor := flag.String("rain_color", "#268bd2",
+		"Color to display text when it's raining")
+	flag.Parse()
+	if *location == "" {
+		location = nil
+	}
+	err := main_loop(location, rainColor)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
-
--- pluie_dans_lheure.go
+++ pluie_dans_lheure.go
@@ -1,219 +1,220 @@
 package main
+
 import (
-    "encoding/json"
-    "errors"
-    "io/ioutil"
-    "net/http"
-    "os"
-    "strings"
-    "syscall"
-    "time"
-    "bytes"
+	"bytes"
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+	"syscall"
+	"time"
 )
+
 const STATUS_LEN int = 9
 
 func get_bearer() (string, error) {
-    var url string = "https://meteofrance.com/previsions-meteo-france/"
+	var url string = "https://meteofrance.com/previsions-meteo-france/"
 
-    clt := http.Client{
-        Timeout: time.Second * 2, // Maximum of 2 secs
-    }
+	clt := http.Client{
+		Timeout: time.Second * 2, // Maximum of 2 secs
+	}
 
-    req, errHttp := http.NewRequest(http.MethodHead, url, nil)
-    if errHttp != nil {
-        return "", errHttp
-    }
-    req.Header.Set("User-Agent", "PluieDansLheureFori3status")
+	req, errHttp := http.NewRequest(http.MethodHead, url, nil)
+	if errHttp != nil {
+		return "", errHttp
+	}
+	req.Header.Set("User-Agent", "PluieDansLheureFori3status")
 
-    res, errGet := clt.Do(req)
-    if errGet != nil {
-        return "", errGet
-    }
+	res, errGet := clt.Do(req)
+	if errGet != nil {
+		return "", errGet
+	}
 
-    setcookie := res.Header.Get("Set-Cookie")
-    if len(setcookie) < 20 {
-        return "", errors.New("set cookie string too short")
-    }
-    if !strings.HasPrefix(setcookie, "mfsession=") {
-        return "", errors.New("set cookie string does not start with mfsession")
-    }
-    setcookie = strings.TrimPrefix(setcookie, "mfsession=")
-    idx := strings.IndexByte(setcookie, ';')
-    if idx < 0 {
-        return "", errors.New("set cookie string does not have a ';'")
-    }
-    buf := bytes.NewBufferString("Bearer ")
-    /* Yes, that's ROT13 … o\ */
-    for i := 0; i < idx; i++ {
-        c := setcookie[i]
-       if c >= 'a' && c <= 'm' || c >= 'A' && c <= 'M' {
-           c += 13
-       } else if c >= 'n' && c <= 'z' || c >= 'N' && c <= 'Z' {
-           c -= 13
-       }
-       buf.WriteByte(c)
-    }
-    return buf.String(), nil
+	setcookie := res.Header.Get("Set-Cookie")
+	if len(setcookie) < 20 {
+		return "", errors.New("set cookie string too short")
+	}
+	if !strings.HasPrefix(setcookie, "mfsession=") {
+		return "", errors.New("set cookie string does not start with mfsession")
+	}
+	setcookie = strings.TrimPrefix(setcookie, "mfsession=")
+	idx := strings.IndexByte(setcookie, ';')
+	if idx < 0 {
+		return "", errors.New("set cookie string does not have a ';'")
+	}
+	buf := bytes.NewBufferString("Bearer ")
+	/* Yes, that's ROT13 … o\ */
+	for i := 0; i < idx; i++ {
+		c := setcookie[i]
+		if c >= 'a' && c <= 'm' || c >= 'A' && c <= 'M' {
+			c += 13
+		} else if c >= 'n' && c <= 'z' || c >= 'N' && c <= 'Z' {
+			c -= 13
+		}
+		buf.WriteByte(c)
+	}
+	return buf.String(), nil
 }
 
 func get_status_from_http(location *string) (string, error) {
-    var url string = "https://rpcache-aa.meteofrance.com/internet2018client/2.0/nowcast/rain?" + *location
+	var url string = "https://rpcache-aa.meteofrance.com/internet2018client/2.0/nowcast/rain?" + *location
 
-    bearer, errBearer := get_bearer()
-    if errBearer != nil {
-        return "", errBearer
-    }
+	bearer, errBearer := get_bearer()
+	if errBearer != nil {
+		return "", errBearer
+	}
 
-    clt := http.Client{
-        Timeout: time.Second * 2, // Maximum of 2 secs
-    }
+	clt := http.Client{
+		Timeout: time.Second * 2, // Maximum of 2 secs
+	}
 
-    req, errHttp := http.NewRequest(http.MethodGet, url, nil)
-    if errHttp != nil {
-        return "", errHttp
-    }
+	req, errHttp := http.NewRequest(http.MethodGet, url, nil)
+	if errHttp != nil {
+		return "", errHttp
+	}
 
-    req.Header.Set("User-Agent", "PluieDansLheureFori3status")
-    req.Header.Set("Authorization", bearer)
+	req.Header.Set("User-Agent", "PluieDansLheureFori3status")
+	req.Header.Set("Authorization", bearer)
 
-    res, errGet := clt.Do(req)
-    if errGet != nil {
-        return "", errGet
-    }
+	res, errGet := clt.Do(req)
+	if errGet != nil {
+		return "", errGet
+	}
 
-    body, errRead := ioutil.ReadAll(res.Body)
-    if errRead != nil {
-        return "", errRead
-    }
+	body, errRead := ioutil.ReadAll(res.Body)
+	if errRead != nil {
+		return "", errRead
+	}
 
-    var data map[string]interface{}
-    errJson := json.Unmarshal(body, &data)
-    if errJson != nil {
-        return "", errJson
-    }
-    var dataProperties, okP = data["properties"].(map[string]interface{})
-    if !okP {
-        return "", errors.New("can not find 'properties' in JSON")
-    }
-    var dataForecast, okF = dataProperties["forecast"].([]interface{})
-    if !okF {
-        return "", errors.New("can not find 'forecast' in 'properties' in JSON")
-    }
-    var b strings.Builder
-    b.Grow(STATUS_LEN * 4 /* to account for unicode character */)
-    for _, v := range dataForecast {
-        var rain, ok = v.(map[string]interface{})
-        if !ok {
-            return "", errors.New("invalid element in 'properties.forecast' in JSON")
-        }
-        var intensity, ok2 = rain["rain_intensity"].(float64)
-        if !ok2 {
-            return "", errors.New("'rain_intensity' not found in sub element in JSON")
-        }
-        switch {
-        case intensity <= 1:
-            b.WriteRune('_')
-        case intensity <= 2:
-            b.WriteRune('░')
-        case intensity <= 3:
-            b.WriteRune('▒')
-        case intensity <= 4:
-            b.WriteRune('▓')
-        default:
-            b.WriteRune('█')
-        }
-    }
+	var data map[string]interface{}
+	errJson := json.Unmarshal(body, &data)
+	if errJson != nil {
+		return "", errJson
+	}
+	var dataProperties, okP = data["properties"].(map[string]interface{})
+	if !okP {
+		return "", errors.New("can not find 'properties' in JSON")
+	}
+	var dataForecast, okF = dataProperties["forecast"].([]interface{})
+	if !okF {
+		return "", errors.New("can not find 'forecast' in 'properties' in JSON")
+	}
+	var b strings.Builder
+	b.Grow(STATUS_LEN * 4 /* to account for unicode character */)
+	for _, v := range dataForecast {
+		var rain, ok = v.(map[string]interface{})
+		if !ok {
+			return "", errors.New("invalid element in 'properties.forecast' in JSON")
+		}
+		var intensity, ok2 = rain["rain_intensity"].(float64)
+		if !ok2 {
+			return "", errors.New("'rain_intensity' not found in sub element in JSON")
+		}
+		switch {
+		case intensity <= 1:
+			b.WriteRune('_')
+		case intensity <= 2:
+			b.WriteRune('░')
+		case intensity <= 3:
+			b.WriteRune('▒')
+		case intensity <= 4:
+			b.WriteRune('▓')
+		default:
+			b.WriteRune('█')
+		}
+	}
 
-    return b.String(), nil
+	return b.String(), nil
 }
 
-func read_status_from_file_no_lock(f *os.File, file_length int64) (string,  error) {
-    var buf = make([]byte, file_length) // to account for unicode characters
-    var _, err = f.Read(buf)
-    if err != nil {
-        return "", err
-    }
-    return string(buf), nil
+func read_status_from_file_no_lock(f *os.File, file_length int64) (string, error) {
+	var buf = make([]byte, file_length) // to account for unicode characters
+	var _, err = f.Read(buf)
+	if err != nil {
+		return "", err
+	}
+	return string(buf), nil
 }
 
-func write_status_to_file_no_lock(f *os.File, status string) (error) {
-    var truncateErr = f.Truncate(0)
-    if truncateErr != nil {
-        return truncateErr
-    }
-    var _, writeErr = f.WriteString(status)
-    if writeErr != nil {
-        return writeErr
-    }
-    var syncErr = f.Sync()
-    return syncErr
+func write_status_to_file_no_lock(f *os.File, status string) error {
+	var truncateErr = f.Truncate(0)
+	if truncateErr != nil {
+		return truncateErr
+	}
+	var _, writeErr = f.WriteString(status)
+	if writeErr != nil {
+		return writeErr
+	}
+	var syncErr = f.Sync()
+	return syncErr
 }
 
-func need_new_status(f *os.File, location *string) (string , error) {
-    var status, err = get_status_from_http(location)
-    if err != nil {
-        var file_path string = "/tmp/pluie_dans_lheure.err"
+func need_new_status(f *os.File, location *string) (string, error) {
+	var status, err = get_status_from_http(location)
+	if err != nil {
+		var file_path string = "/tmp/pluie_dans_lheure.err"
 
-        var f, openErr = os.OpenFile(file_path, os.O_RDWR|os.O_CREATE, 0644)
-        if openErr != nil {
-            return "", nil
-        }
-        defer f.Close()
-        f.WriteString(err.Error())
-        return "", err
-    }
-    var writeErr = write_status_to_file_no_lock(f, status)
-    if writeErr != nil {
-        return "", writeErr
-    }
-    return status, nil
+		var f, openErr = os.OpenFile(file_path, os.O_RDWR|os.O_CREATE, 0644)
+		if openErr != nil {
+			return "", nil
+		}
+		defer f.Close()
+		f.WriteString(err.Error())
+		return "", err
+	}
+	var writeErr = write_status_to_file_no_lock(f, status)
+	if writeErr != nil {
+		return "", writeErr
+	}
+	return status, nil
 }
-
 
 func GetRain(location *string) (string, error) {
-    var file_path string = "/tmp/pluie_dans_lheure"
+	var file_path string = "/tmp/pluie_dans_lheure"
 
-    var f, openErr = os.OpenFile(file_path, os.O_RDWR|os.O_CREATE, 0644)
-    if openErr != nil {
-        return "", nil
-    }
-    defer f.Close()
-    var fd = f.Fd()
+	var f, openErr = os.OpenFile(file_path, os.O_RDWR|os.O_CREATE, 0644)
+	if openErr != nil {
+		return "", nil
+	}
+	defer f.Close()
+	var fd = f.Fd()
 
-    /* Flock */
-    var flockErr = syscall.Flock(int(fd), syscall.LOCK_EX)
-    if flockErr != nil {
-        return "",  nil
-    }
-    defer syscall.Flock(int(fd), syscall.LOCK_UN)
+	/* Flock */
+	var flockErr = syscall.Flock(int(fd), syscall.LOCK_EX)
+	if flockErr != nil {
+		return "", nil
+	}
+	defer syscall.Flock(int(fd), syscall.LOCK_UN)
 
-    var st, statErr = f.Stat()
-    if statErr != nil {
-        return "", nil
-    }
-    var t_hour, t_min, _ int = st.ModTime().Clock()
-    var now_hour, now_min, _ int = time.Now().Clock()
-    if st.Size() < int64(STATUS_LEN) || t_hour != now_hour || t_min/5 != now_min/5 {
-        return need_new_status(f, location)
-    }
-    return read_status_from_file_no_lock(f, st.Size())
+	var st, statErr = f.Stat()
+	if statErr != nil {
+		return "", nil
+	}
+	var t_hour, t_min, _ int = st.ModTime().Clock()
+	var now_hour, now_min, _ int = time.Now().Clock()
+	if st.Size() < int64(STATUS_LEN) || t_hour != now_hour || t_min/5 != now_min/5 {
+		return need_new_status(f, location)
+	}
+	return read_status_from_file_no_lock(f, st.Size())
 }
 
 func GetRainI3barFormat(location *string, rain_color *string) (string, error) {
-    status, rainErr := GetRain(location)
-    if rainErr != nil {
-        return "", rainErr
-    }
-    /* Poor man's json encoder */
-    var b strings.Builder
-    b.WriteString("{\"full_text\":\"")
-    b.WriteString(status)
-    b.WriteString("\"")
-    if status != "____________" {
-        b.WriteString(",\"color\":\"")
-        b.WriteString(*rain_color)
-        b.WriteString("\"")
-    }
-    b.WriteString("}")
-    return b.String(), nil
+	status, rainErr := GetRain(location)
+	if rainErr != nil {
+		return "", rainErr
+	}
+	/* Poor man's json encoder */
+	var b strings.Builder
+	b.WriteString("{\"full_text\":\"")
+	b.WriteString(status)
+	b.WriteString("\"")
+	if status != "____________" {
+		b.WriteString(",\"color\":\"")
+		b.WriteString(*rain_color)
+		b.WriteString("\"")
+	}
+	b.WriteString("}")
+	return b.String(), nil
 }
